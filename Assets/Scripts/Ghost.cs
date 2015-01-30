06c6e8891e19627fc65d750d926a30add8b21bc4
TheTemportalist <the.country.gamer@gmail.com>
2015-01-30 10:33:50 -0500

    Worked on ghosts

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : MovingEntity {
+	
+	public float speed = 0.3f;
+
+	private Transform start;
+	private Transform goal;
+	private Vector2[] dirs = new Vector2[]{Vector2.up, Vector2.right, -Vector2.up, -Vector2.right};
+
+	void Start() {
+		this.start = this.transform;
+		this.goal = this.transform;
+
+		this.setGoal (new Vector2(0f, 3f));
+
+	}
+
+	void FixedUpdate() {
+		if (!this.hasHitGoal ()) {
+			print ("findnew");
+			//this.findNewGoal ();
+		} else {
+			//print ("move to");
+			this.moveTowardsGoal ();
+		}
+
+		// Animation
+		Vector2 dir = this.goal.position - this.transform.position;
+		GetComponent<Animator>().SetFloat("DirX", dir.x);
+		GetComponent<Animator>().SetFloat("DirY", dir.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D co) {
+		if (co.name == "pacman")
+			Destroy(co.gameObject);
+	}
+
+	void setNextGoal() {
+
+	}
+
+	void setGoal(Vector2 pos) {
+		this.goal.position = pos;
+	}
+
+	bool hasHitGoal() {
+		//print (this.transform.position);
+		//print (this.goal.position);
+		return this.transform.position == this.goal.position;
+	}
+
+	void findNewGoal() {
+		this.goal.position = this.getRandomDir (1);
+	}
+
+	Vector2 getRandomDir(float length) {
+		Vector2 dir;
+		do {
+			dir = this.dirs [Random.Range (0, this.dirs.Length)];
+			dir.x *= length;
+			dir.y *= length;
+		} while (!this.isValidDirection(dir));
+		return dir;
+	}
+
+	void moveTowardsGoal() {
+		print (this.moveTo(this.goal.position, this.speed));
+	}
+
+
+}
--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pacman : MonoBehaviour {
+public class Pacman : MovingEntity {
 
 	// speed variable to contorl how fast pacman moves
 	public float speed = 0.4f;
@@ -35,36 +35,6 @@ public class Pacman : MonoBehaviour {
 		GetComponent<Animator>().SetFloat("DirX", dir.x);
 		GetComponent<Animator>().SetFloat("DirY", dir.y);
 
-	}
-	
-	// Cast Line from 'next to Pac-Man' to 'Pac-Man'
-	bool isValidDirection(Vector2 dir) {
-		// We check the SIDES of the pacman, because checking just the center
-		// could result in us being too thick for the current point of entry.
-		//		A linecast (in checkCollision) can hit us,
-		//		even if the line is right on the edge of a collider.
-		if (dir.y == 0) { // along x axis
-			Vector2 top = (Vector2)this.transform.position + new Vector2(0f, 0.5f);
-			Vector2 bottom = (Vector2)this.transform.position + new Vector2(0f, -0.5f);
-			return this.checkCollision(top, dir) && this.checkCollision(bottom, dir);
-		}
-		else { // along y axis
-			Vector2 left = (Vector2)this.transform.position + new Vector2(-0.5f, 0f);
-			Vector2 right = (Vector2)this.transform.position + new Vector2(0.5f, 0f);
-			return this.checkCollision(left, dir) && this.checkCollision(right, dir);
-		}
-		return false;
-	}
-
-	bool checkCollision(Vector2 pos, Vector2 dir) {
-		// this is a layer mask. 8 is the pacdot layer, so 1 << 8 is the mask.
-		// to ignore that layer, we invert the mask
-		int layerMask = ~(1 << 8);
-		// cast from the target to this object
-		// if we hit something else, then the point is behind an object. otherwise, the line hits ourself
-		RaycastHit2D hit = Physics2D.Linecast (pos + dir, pos, layerMask);
-		// if it hits this collider, than there is nothing in the way
-		return (hit.collider == this.collider2D);
 	}
 
 	void checkKeys() {
