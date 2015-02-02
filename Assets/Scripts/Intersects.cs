ab9e6c3efdaedb41d2e24df3d208fd8599e709ca
TheTemportalist <the.country.gamer@gmail.com>
2015-02-01 22:11:43 -0500

    Moving things about and reworking for more modularity

---
+++ Assets/Scripts/Intersects.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Intersects : ObjCreator {
+
+	public bool generate = true;
+
+	override protected bool isEnabled() {
+		return this.generate;
+	}
+
+	override protected Dictionary<int[], int[]> getPoints () {
+		return new Dictionary<int[], int[]> (){
+			{new int[]{12, 15}, new int[]{1}},
+			{new int[]{3, 24}, new int[]{4}},
+			{new int[]{6, 9, 18, 21}, new int[]{7}},
+			{new int[]{6, 9, 18, 21}, new int[]{10}},
+			{new int[]{9, 18}, new int[]{13}},
+			{new int[]{6, 9, 18, 21}, new int[]{16}},
+			{new int[]{6, 21}, new int[]{22}},
+			{new int[]{1, 6, 9, 12, 15, 18, 21, 26}, new int[]{25}},
+			{new int[]{6, 21}, new int[]{29}}
+		};
+	}
+
+	override protected void createAtPoint(Vector3 point) {
+		GameObject empty = new GameObject ("intersect");
+		IconManager.SetIcon (empty, IconManager.Icon.DiamondRed);
+		empty.transform.parent = this.transform;
+		empty.transform.position = point;
+	}
+
+}
---
+++ Assets/Scripts/ObjCreator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class ObjCreator : MonoBehaviour {
+
+	protected abstract bool isEnabled ();
+
+	protected virtual void init() {}
+
+	protected abstract Dictionary<int[], int[]> getPoints ();
+
+	protected abstract void createAtPoint(Vector3 point);
+
+	// Use this for initialization
+	public void Start () {
+		if (!this.isEnabled ()) return;
+		this.init ();
+		foreach (KeyValuePair<int[], int[]> points in this.getPoints()) {
+			foreach (int x in points.Key) {
+				foreach (int y in points.Value) {
+					this.createAtPoint(new Vector3(x, y) + new Vector3(.5f, .5f));
+				}
+			}
+		}
+	}
+
+}
+
